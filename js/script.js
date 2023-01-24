fb029e29a42b533ed771c65dbfff9910c363c078
GiadaMarzapane <giada.ortesta@gmail.com>
2023-01-24 15:46:25 +0100

    Push MILESTONE1

--- js/script.js
+++ js/script.js
@@ -3,7 +3,21 @@ const { createApp } = Vue;
 createApp({
     data(){
         return{
-            message: 'hello'
+            myTasks: [
+                {
+                    text: 'Fare la spesa',
+                    done: true
+                },{
+                    text: 'Pagare il bollo',
+                    done: false
+                },{
+                    text: 'Lavare la macchina',
+                    done: true
+                }, {
+                    text: 'Comprare regalo Martina',
+                    done: false
+                }
+            ]
         }
     },
     methods:{
